3b82be38f390bb2a0d8c7d1dd026690913174ff2
daesungra <daesungra@gmail.com>
2022-11-30 22:34:55 +0900

    tutorial2: Add api_view wrapper and Set rest_framework's Request, Response module

--- apps/snippets/views.py
+++ apps/snippets/views.py
@@ -1,18 +1,31 @@
 """
 Views of snippets app.
+    rest_framework.request.Request:
+        The core functionality of the ```Request``` object is the ```request.data``` attribute,
+        which is similar to ```request.POST```, but more useful for working with Web APIs.
+    rest_framework.response.Response:
+        This is a type of ```TemplateResponse``` that takes unrendered content
+        and uses content negotiation to determine the correct content type to return to the client.
+    rest_framework.status:
+        REST framework provides more explicit identifiers for each status code,
+        such as ```HTTP_400_BAD_REQUEST``` in this module.
+        It's a good idea to use these throughout rather than using numeric identifiers.
+    rest_framework.decorators.api_view:
+        This wrapper provides a few bits of functionality such as making sure
+        you receive ```Request``` instances in your view,
+        and adding context to ```Response``` objects so that content negotiation can be performed.
 """
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
-@csrf_exempt
-def snippet_list(request):
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
         :param request: Http request object.
@@ -21,18 +34,30 @@ def snippet_list(request):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+
+        return Response(serializer.data)
+        # return JsonResponse(serializer.data, safe=False)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        """
+        rest_framework.request.Request:
+            This object has ```request.data``` attribute
+            that is commonly used in 'POST', 'PUT', and 'DELETE' methods.
+        """
+        serializer = SnippetSerializer(data=request.data)
+        # data = JSONParser().parse(request)
+        # serializer = SnippetSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #     return JsonResponse(serializer.data, status=201)
+        # return JsonResponse(serializer.errors, status=400)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
         :param request: Http request object.
@@ -42,18 +67,33 @@ def snippet_detail(request, pk):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        # return HttpResponse(status=404)
 
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+
+        return Response(serializer.data)
+        # return JsonResponse(serializer.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        """
+        rest_framework.request.Request:
+            This object has ```request.data``` attribute
+            that is commonly used in 'POST', 'PUT', and 'DELETE' methods.
+        """
+        serializer = SnippetSerializer(snippet, data=request.data)
+        # data = JSONParser().parse(request)
+        # serializer = SnippetSerializer(snippet, data=data)
+
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        #     return JsonResponse(serializer.data)
+        # return JsonResponse(serializer.errors, status=400)
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        # return HttpResponse(status=204)
