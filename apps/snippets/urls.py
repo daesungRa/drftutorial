e6df5e68bbdbeada3a1e975affa6d06094f860ce
daesungra <daesungra@gmail.com>
2022-11-30 22:36:23 +0900

    tutorial2: Set format_suffix_patterns to snippets url

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -4,6 +4,8 @@ URLConf of snippet app.
 
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from .views import snippet_list, snippet_detail
 
 
@@ -12,3 +14,5 @@ urlpatterns = [
     path('', snippet_list),
     path('<int:pk>/', snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns=urlpatterns)
