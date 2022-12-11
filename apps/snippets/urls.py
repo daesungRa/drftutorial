281a5a030dfefedef36da579bd78a5df629d3682
daesungra <daesungra@gmail.com>
2022-12-11 16:36:49 +0900

    tutorial6: Set DRF routers to urlconf of snippet app

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -2,20 +2,57 @@
 URLConf of snippet app.
 """
 
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import api_root, SnippetHighlight, UserList, UserDetail, SnippetList, SnippetDetail
+from rest_framework.routers import DefaultRouter
+
+from ..quickstart.views import UserViewSet as QuickstartUserViewSet
+from .views import UserViewSet, SnippetViewSet, api_root
 
 
-urlpatterns = format_suffix_patterns(urlpatterns=[
+# Tutorial6: Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet, basename='snippet')
+router.register(r'user-snippets', UserViewSet, basename='user-snippet')
+router.register(r'users', QuickstartUserViewSet, basename='users')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+# DEPRECATED!!
+# Notice: "Expected type 'ViewSetMixin'" warning is caused by specific version of DRF or PyCharm editor.
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy',
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight',
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list',
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+})
+Deprecated_urlpatterns = format_suffix_patterns(urlpatterns=[
     path(r'', api_root),
-    path(r'snippets/', SnippetList.as_view(), name='snippet-list'),
-    path(r'snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path(r'snippets/', snippet_list, name='snippet-list'),
+    path(r'snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
     path(r'snippets/<int:pk>/highlight/',
-         SnippetHighlight.as_view(),
+         snippet_highlight,
          name='snippet-highlight'),  # Using HTML renderer from REST framework
-    path(r'users/', UserList.as_view(), name='snippet-user-list'),
-    path(r'users/<int:pk>/', UserDetail.as_view(), name='snippet-user-detail'),
+    path(r'users/', user_list, name='snippet-user-list'),
+    path(r'users/<int:pk>/', user_detail, name='snippet-user-detail'),
 ])
