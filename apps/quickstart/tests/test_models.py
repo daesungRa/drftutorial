94721b3e140654bf45e6e05e2ca2dbbca31dd2aa
daesungra <daesungra@gmail.com>
2022-12-17 21:23:32 +0900

    test: Add model test module to quickstart app

---
+++ apps/quickstart/tests/test_models.py
@@ -0,0 +1,42 @@
+"""
+Test models in quickstart app.
+"""
+
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+
+from .mixins import UserTestMixin
+
+
+class UserTest(UserTestMixin, TestCase):
+    """
+    Test module for Django User model.
+    """
+
+    def test_get_user(self) -> None:
+        """User model test"""
+        user = User.objects.get(username=self.username)
+
+        # Default info check
+        self.assertEqual(user.email, self.email)
+
+        # User password check
+        self.assertEqual(user.check_password(self.password), True)
+        self.assertEqual(user.check_password('ttt02'), False)
+
+    def test_create_user(self) -> None:
+        """User creation test"""
+        email = 't2@test.py'
+        username = 'tuser02'
+        password = 'ttt02'
+        self._create_test_user(email=email, username=username, password=password)
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return
+
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.check_password(password), True)
+        self.assertEqual(user.check_password('ttt03'), False)
